f343761b7ec1c08e44235e4aa5374993b555a6ff
elenamazyrina <mazyrina.mail@gmail.com>
2026-05-19 23:19:43 +0300

    working with days calculation: general dates calc

--- src/app/progress-bar/progress-bar.ts
+++ src/app/progress-bar/progress-bar.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ElementRef, inject} from '@angular/core';
+import {differenceInDays, format} from 'date-fns';
 
 @Component({
   selector: 'app-progress-bar',
@@ -7,13 +8,18 @@ import {AfterViewInit, Component, ElementRef, inject} from '@angular/core';
   styleUrl: './progress-bar.css',
 })
 export class ProgressBar implements AfterViewInit {
-  private readonly el: ElementRef<any> = inject(ElementRef);
+  private readonly nowDate = new Date(2026, 4, 19);
+  private readonly goalDate = new Date(2026, 8, 3);
+  private readonly el = inject(ElementRef);
 
   ngAfterViewInit(): void {
+    const fullDurationInDays = differenceInDays(this.goalDate, this.nowDate);
+    const currentDurationInDays = differenceInDays(this.goalDate, Date.now());
+    console.log(format(this.nowDate, 'yyyy.MM.dd'));
+    console.log(format(Date.now(), 'yyyy.MM.dd'));
     this.setProgress(10);
-    }
+  }
 
-  // Function to update progress
   private setProgress(percent: number): void {
     const bar = this.el.nativeElement.querySelector('.progressBar');
     bar.style.width = `${Math.min(100, Math.max(0, percent))}%`;
