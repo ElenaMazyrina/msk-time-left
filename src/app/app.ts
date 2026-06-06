1843b485ddd2a1ab2119232b8b582288fc1bdff7
elenamazyrina <mazyrina.mail@gmail.com>
2026-06-07 00:57:36 +0300

    page loading refactoring

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,9 @@ import {ProgressBar} from './progress-bar/progress-bar';
   standalone: true,
 })
 export class App {
+  constructor() {
+    window.addEventListener('load', function() {
+      document.body.classList.add('loaded');
+    });
+  }
 }
