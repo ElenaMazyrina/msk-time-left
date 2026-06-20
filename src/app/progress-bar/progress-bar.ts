035e37da93f363af08cd4b5c2685d0593ee1060f
elenamazyrina <mazyrina.mail@gmail.com>
2026-06-21 00:07:57 +0300

    adding relaxing mode

--- src/app/progress-bar/progress-bar.ts
+++ src/app/progress-bar/progress-bar.ts
@@ -1,5 +1,6 @@
-import {AfterViewInit, Component, ElementRef, inject} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, inject, OnDestroy, signal} from '@angular/core';
 import {TimeDownCalculationService} from '../time-down-calculation-service';
+import {Subject, takeUntil, timer} from 'rxjs';
 
 @Component({
   selector: 'app-progress-bar',
@@ -7,17 +8,31 @@ import {TimeDownCalculationService} from '../time-down-calculation-service';
   templateUrl: './progress-bar.html',
   styleUrl: './progress-bar.css',
 })
-export class ProgressBar implements AfterViewInit {
+export class ProgressBar implements AfterViewInit, OnDestroy {
   private readonly el = inject(ElementRef);
   private readonly timeDownCalculationService = inject(TimeDownCalculationService);
   protected readonly durationInDays = this.timeDownCalculationService.getDifferenceInDays();
+  protected isSofaVisible = signal<boolean>(false);
+  private destroy$: Subject<void> = new Subject<void>();
 
   ngAfterViewInit(): void {
     this.setProgress(this.timeDownCalculationService.getDiffInPercentage());
   }
 
+  protected showSofa(): void {
+    this.isSofaVisible.set(true);
+    timer(1000 * 10)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.isSofaVisible.set(false));
+  }
+
   private setProgress(percent: number): void {
     const bar = this.el.nativeElement.querySelector('.progress-bar');
     bar.style.width = `${Math.min(100, Math.max(0, percent))}%`;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
